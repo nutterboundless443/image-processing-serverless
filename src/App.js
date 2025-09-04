ab8149277eed29fb8e5ede1288b9a6675397839a
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-04 12:46:26 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageUpload from './components/ImageUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>图像处理无服务器平台</h1>
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default App;
