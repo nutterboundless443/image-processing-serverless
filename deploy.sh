669b3029e93e1f4c1632197c0190a77724375a0b
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-04 12:48:23 -0500

    Update S3 bucket name for deployment

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@
 npm run build
 
 # 部署到S3
-aws s3 sync build/ s3://my-updated-s3-bucket-name/
+aws s3 sync build/ s3://new-bucket-name/
 
 # 更新Lambda函数
 aws lambda update-function-code \
     --function-name your-function-name \
-    --s3-bucket my-updated-s3-bucket-name \
+    --s3-bucket new-bucket-name \
     --s3-key index.zip
 
 echo "部署完成！"
