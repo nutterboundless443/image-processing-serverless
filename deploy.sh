30faf8fbe37eebaccc9af4d86825e261bec8f37c
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-04 12:46:23 -0500

    feat: initial commit for deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 部署到AWS Lambda的脚本
+
+# 打包项目
+npm run build
+
+# 部署到S3
+aws s3 sync build/ s3://your-s3-bucket-name/
+
+# 更新Lambda函数
+aws lambda update-function-code --function-name your-function-name --s3-bucket your-s3-bucket-name --s3-key index.zip
+
+echo "部署完成！"
